7caa25caa0b40eabd1fefdb464a6740632e327c2
Zana Canpolat <zana_canpolat@hotmail.se>
2025-01-14 14:31:37 +0100

    Exercise switching out a word inside a test string

--- src/main/java/org/Lexicon/group54/Main.java
+++ src/main/java/org/Lexicon/group54/Main.java
@@ -30,26 +30,25 @@ public class Main
 
         System.out.println(); // Printing an empty line to increase visibility
 
-        //3. Creating an sub string and printing aout
+        //3. Creating an sub string and printing out
         testString = "Ok this is not as long!";
         System.out.println("Printing teststring = " + testString);
-        System.out.println(" = " + testString.substring(11,22));
+        System.out.println("Creating a substring = " + testString.substring(11,22)); // there is several ways of doing this, search for the word you are looking for, keep track of position
 
         System.out.println(); // Printing an empty line to increase visibility
 
         //4. Converting caps to lowercase and back again
         testString = "CAPS EQUALS SCREAMING!";
         System.out.println("Printing teststring = " + testString);
-        System.out.println("testString.indexOf(o) = " + testString.toLowerCase());
-        System.out.println("testString.indexOf(o) = " + testString.toUpperCase());
+        System.out.println("Converting all caps to lower case = " + testString.toLowerCase());
+        System.out.println("Converting lowercase to caps = " + testString.toUpperCase());
 
         System.out.println(); // Printing an empty line to increase visibility
 
         //5. Switching out a word inside a string
         testString = "Java is the worst programming language!";
-        System.out.println("Printing teststring = " + testString);
-        System.out.println("testString.indexOf(o) = " + testString.toLowerCase());
-        System.out.println("testString.indexOf(o) = " + testString.toUpperCase());
+        System.out.println("Printing test string = " + testString);
+        System.out.println("Changing a word inside a text string = " + testString.replace("worst", "best"));
 
     }
 }
